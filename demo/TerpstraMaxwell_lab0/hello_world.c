e7b698f9e3072da01d2744527e272cb5eb518fca
Maxwell Terpstra <telic.detour+github@gmail.com>
2020-09-05 15:48:06 -0700

    first draft of lab-0

---
+++ demo/TerpstraMaxwell_lab0/hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    puts("Hello world!");
+    puts("My name is Maxwell Terpstra");
+    puts("I'm the instructor for this course.");
+    puts("I hope that by the end of the semester I will have:");
+    puts(" - learned everyone's names");
+    puts(" - inspired you to continue learning about computer science");
+    puts(" - taught you enough to start building some real programs!");
+}
